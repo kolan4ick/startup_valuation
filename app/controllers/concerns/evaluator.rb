7b285e71fc4911ea96d01d8b074f6f086a25b32c
kolan4ick <Kolan4ick@gmail.com>
2023-11-12 03:17:07 +0200

    Add risk evaluation to app
    Modified the existing Rails app by adding routes, controller actions, views and supporting calculations for risk evaluation. The new feature allows the application to perform a complex risk analysis, displaying the data in a visual table format. The purpose of this change is to give users more comprehensive data to help them make informed decisions. The new logical components are highly modular and reusable, promoting the DRY principle for future developers.

---
+++ app/controllers/concerns/evaluator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Evaluator
+  def initialize; end
+
+  # Evaluate the effectiveness of the startup
+  def evaluate
+    raise NotImplementedError
+  end
+end
--- app/controllers/evaluation_controller.rb
+++ app/controllers/evaluation_controller.rb
@@ -1,12 +1,47 @@
 class EvaluationController < ApplicationController
   def index
-    @data = [
+    @data_1 = [
       [70.0, 50.0, 40.0, 150.0, 65.0],
       [20.0, 15.0, 10.0, 50.0, 25.0],
       [115.0, 60.0, 50.0, 225.0, 90.0],
       [80.0, 55.0, 35.0, 165.0, 50.0],
       [3, 3, 5, 4, 3],
       [10, 8, 6, 7, 4]
+    ]
+
+    @data_2 = [
+      [
+        ["Н", 0.8],
+        ["Н", 0.7],
+        ["НС", 0.9],
+        ["Н", 0.6],
+        ["НС", 0.7],
+        ["С", 0.5],
+        ["Н", 0.7],
+        ["Н", 0.8],
+        ["Н", 0.9],
+      ],
+      [
+        ["НС", 0.7],
+        ["Н", 0.5],
+        ["С", 0.6],
+        ["НС", 0.8],
+        ["НС", 0.9],
+      ],
+      [
+        ["НС", 0.3],
+        ["НС", 0.6],
+        ["НС", 0.2],
+        ["Н", 0.7],
+        ["Н", 0.6],
+      ],
+      [
+        ["Н", 0.8],
+        ["Н", 0.9],
+        ["НС", 0.1],
+        ["НС", 0.7],
+        ["НС", 0.6],
+      ]
     ]
   end
 
@@ -28,6 +63,20 @@ class EvaluationController < ApplicationController
     @max = @result[:max].map { | i | i.round(3) }
     @m = @result[:m].round(4)
     @linguistic = @result[:linguistic]
+
+    respond_to do | format |
+      format.js
+    end
+  end
+
+  def risk
+    # Data from the form
+    k1 = params[:evaluation][:k1]
+    k2 = params[:evaluation][:k2]
+    k3 = params[:evaluation][:k3]
+    k4 = params[:evaluation][:k4]
+
+    @result = RiskEvaluator.new(k1: k1, k2: k2, k3: k3, k4: k4).evaluate
 
     respond_to do | format |
       format.js
