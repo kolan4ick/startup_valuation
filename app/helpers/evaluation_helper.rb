9ab4310dc5228689609547882999a0d2fb2d5eed
kolan4ick <Kolan4ick@gmail.com>
2024-02-20 18:59:10 +0200

    Add style and javascript to dynamically change the input table on different methods

--- app/helpers/evaluation_helper.rb
+++ app/helpers/evaluation_helper.rb
@@ -1,11 +1,11 @@
 module EvaluationHelper
-  def multicriteria_input(position, collection, value=nil)
+  def multicriteria_input(position, collection, value = nil)
     if collection.nil?
       text_field_tag "evaluation[x#{position + 1}][]", value, class: "form-control", type: :text
     else
       value = collection.index(value) if value
 
-      select_tag "evaluation[x#{position + 1}][]", options_for_select(collection.each_with_index.map { |e, idx| [e, idx] }, value), value: value, class: "form-control"
+      select_tag "evaluation[x#{position + 1}][]", options_for_select(collection.each_with_index.map { | e, idx | [e, idx] }, value), value: value, class: "form-control"
     end
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,7 @@ import "@rails/ujs";
 import "bootstrap";
 
 import "@fortawesome/fontawesome-free/js/all";
+
 document.addEventListener("turbo:load", () => {
     document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('data-bs-theme') || 'light');
 });
@@ -23,3 +24,23 @@ function handleThemeSwitching(theme) {
     document.documentElement.setAttribute('data-bs-theme', theme);
     localStorage.setItem('data-bs-theme', theme);
 }
+
+function changeMethod(element) {
+    let method = element.value;
+
+    const multicriteriaHideableTh = document.getElementsByClassName("multicriteria-hideable-th")[0];
+    const multicriteriaHideableTd = document.getElementsByClassName("multicriteria-hideable-td");
+    if (method === "0") {
+        multicriteriaHideableTh.hidden = true;
+        for (let i = 0; i < multicriteriaHideableTd.length; i++) {
+            multicriteriaHideableTd[i].hidden = true;
+        }
+    } else {
+        multicriteriaHideableTh.hidden = false;
+        for (let i = 0; i < multicriteriaHideableTd.length; i++) {
+            multicriteriaHideableTd[i].hidden = false;
+        }
+    }
+}
+
+window.changeMethod = changeMethod;
